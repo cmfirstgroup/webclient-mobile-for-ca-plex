429cdb12403ddd63d0eeeeedb31bff8b7bd69c65
jkerns <jkerns@9c9c8bb5-657a-3f56-d005-a2b41258e0d8>
2011-05-16 04:28:44 +0000

    Updating Mobile javascript for new dynamic panel generator

---
+++ javascriptmobile+/js/wcli/SmartPanel.js
@@ -0,0 +1,103 @@
+Ext.ns('wcli');
+
+(function() {
+	function _getMappedItem(name, map, items, def) {
+		if (!map[name]) {
+			map[name] = def;
+			items.push(def);
+		}
+		return map[name];
+	}
+	
+	function _alignToolbar(toolbar) {
+		var left = toolbar.items.filter(function(n) {
+			return (n.controlName.align||'left') === 'left';
+		});
+		var center = toolbar.items.filter(function(n) {
+			return n.controlName.align === 'center';
+		});
+		var right = toolbar.items.filter(function(n) {
+			return n.controlName.align === 'right';
+		});
+		var spacer = { xtype: 'spacer' };
+		
+		return left.concat([spacer], center, [spacer], right);
+	}
+	
+	function _buildToolbarConfig() {}
+	
+	function _buildPanelConfig(plexConfig) {
+		var config = {},
+			body = plexConfig.body,
+			fullscreen = plexConfig.fullscreen,
+			toolbars = plexConfig.toolbars;
+		
+		// Toolbar items (always visible)
+		config.dockedItems = [];
+		var tbItems = [],
+			tbMap = {};
+		for (var i = 0; i < toolbars.length; i++) {
+			var item = toolbars[i],
+				num = item.controlName.toolbarNum,
+				toolbar = _getMappedItem(num, tbMap, tbItems, {
+					num: parseInt(num),
+					items: []
+				});
+			toolbar.items.push(item);
+		}
+		tbItems.sort(function(a, b) { return a.num - b.num; });
+		
+		for (var i = 0; i < tbItems.length; i++) {
+			config.dockedItems.push({
+				xtype: 'toolbar',
+				dock: 'top',
+				scroll: 'horizontal',
+				items: _alignToolbar(tbItems[i])
+			});
+		}
+		
+		// Body items (only visible if the panel isn't "fullscreen")
+		config.items = [];
+		if (fullscreen.length === 0) {
+			var bodyItems = [],
+				bodyMap = {};
+			for (var i = 0; i < body.length; i++) {
+				var item = body[i],
+					name = item.controlName.fieldSet,
+					fieldSet = _getMappedItem(name, bodyMap, bodyItems, {
+						name: fieldSetName,
+						items: []
+					});
+				fieldSet.items.push(item);
+			}
+			
+			for (var i = 0; i < bodyItems.length; i++) {
+				config.items.push({
+					xtype: 'fieldset',
+					title: bodyItems[i].name.trim() || undefined,
+					items: bodyItems[i].items
+				});
+			}
+		}
+		
+		// Fullscreen items.  Only display the first one instantiated (can't
+		// have more than one fullscreen control at a time!)
+		else {
+			config.bodyMargin = config.bodyPadding = 0;
+			config.items.push(fullscreen[0]);
+		}
+		
+		return config;
+	}
+	
+	wcli.SmartPanel = Ext.extend(Ext.form.FormPanel, {
+		constructor: function(config) {
+			var plexConfig = config.plexConfig;
+			Ext.apply(config, _buildPanelConfig(plexConfig));
+			
+			wcli.SmartPanel.superclass.constructor.call(this, config);
+		}
+	});
+})();
+
+Ext.reg('smartpanel', wcli.SmartPanel);
