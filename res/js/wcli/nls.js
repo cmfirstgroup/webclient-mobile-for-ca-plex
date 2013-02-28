61921d0781a559b92e4fa2f8dbdae83ed9b72969
lily.taharudin@gmail.com <lily.taharudin@gmail.com@9c9c8bb5-657a-3f56-d005-a2b41258e0d8>
2013-02-28 19:25:21 +0000

    add nls.month

--- res/js/wcli/nls.js
+++ res/js/wcli/nls.js
@@ -1,6 +1,7 @@
 Ext.ns('nls');
 Ext.ns('nls.msg');
 Ext.ns('nls.picker');
+Ext.ns('nls.month');
 
 nls.language = window.navigator.language.substr(0, 2);
 
@@ -66,3 +67,80 @@ nls.picker.nl = {
 };
 
 Ext.apply(nls.picker, nls.picker[nls.language]);
+
+nls.month.en = {
+		1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December"	
+};
+
+nls.month.de = {
+		1: "Januar",
+        2: "Februar",
+        3: "März",
+        4: "April",
+        5: "Mai",
+        6: "Juni",
+        7: "Juli",
+        8: "August",
+        9: "September",
+        10: "Oktober",
+        11: "November",
+        12: "Dezember"	
+};
+
+nls.month.it = {
+		1: "gennaio",
+        2: "febbraio",
+        3: "marzo",
+        4: "aprile",
+        5: "maggio",
+        6: "giugno",
+        7: "luglio",
+        8: "agosto",
+        9: "settembre",
+        10: "ottobre",
+        11: "novembre",
+        12: "dicembre"	
+};
+
+nls.month.fr = {
+		1: "janvier",
+        2: "février",
+        3: "mars",
+        4: "avril",
+        5: "mai",
+        6: "juin",
+        7: "juillet",
+        8: "août",
+        9: "septembre",
+        10: "octobre",
+        11: "novembre",
+        12: "décembre"	
+};
+
+nls.month.nl = {
+		1: "januari",
+        2: "februari",
+        3: "maart",
+        4: "april",
+        5: "mei",
+        6: "juni",
+        7: "juli",
+        8: "augustus",
+        9: "september",
+        10: "october",
+        11: "november",
+        12: "december"	
+};
+
+Ext.apply(nls.month, nls.month[nls.language]);
