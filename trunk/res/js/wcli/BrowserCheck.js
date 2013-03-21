a6ce2f7cdab6ff2d6be9dd9474796e27a7ad1b90
lily.taharudin@gmail.com <lily.taharudin@gmail.com@9c9c8bb5-657a-3f56-d005-a2b41258e0d8>
2013-03-21 23:49:00 +0000

    add check for desktop chrome version to override forward button ui and move BrowserCheck.js script tag after sencha touch is loaded

--- trunk/res/js/wcli/BrowserCheck.js
+++ trunk/res/js/wcli/BrowserCheck.js
@@ -1,3 +1,7 @@
 if(navigator.appVersion.indexOf("Chrome") != -1){
 	document.getElementsByTagName("html")[0].className = "st_chrome";
 }
+if(Ext.is.Desktop == true && navigator.appVersion.indexOf("Chrome") != -1){
+	document.getElementsByTagName("html")[0].className = "st_chromeDesktop";
+}
+
