c62903982d3f10cd1ca0d0401fc71d45d1f0f1cf
lily.taharudin@gmail.com <lily.taharudin@gmail.com@9c9c8bb5-657a-3f56-d005-a2b41258e0d8>
2013-07-12 22:42:05 +0000

    recommit files from repo

--- res/js/wcli/CheckboxToggle.js
+++ res/js/wcli/CheckboxToggle.js
@@ -1,11 +1,16 @@
 Ext.ns('wcli');
+Ext.ns('wcli.field');
 
 /**
  * The default Toggle control is really just a slider and as such operates
  * on integer values.  This extension allows us to specify a transformation
  * function that maps these integer values to Plex values.
  */
-wcli.CheckboxToggle = Ext.extend(Ext.form.Toggle, {
+Ext.define('wcli.field.CheckboxToggle',{
+	extend: 'Ext.field.Toggle', 
+	config: {
+		controlName: {}
+	},
 	checkedValue: 1,
 	uncheckedValue: 0,
 	
@@ -19,7 +24,7 @@ wcli.CheckboxToggle = Ext.extend(Ext.form.Toggle, {
 	
 	constrain: function(value) {
 		if (typeof value === 'number') {
-			return wcli.CheckboxToggle.superclass.constrain.call(this, value);
+			return wcli.form.CheckboxToggle.superclass.constrain.call(this, value);
 		}
 		else {
 			return value;
@@ -30,13 +35,11 @@ wcli.CheckboxToggle = Ext.extend(Ext.form.Toggle, {
 		if (typeof value === 'string') {
 			value = this.getNumber(value);
 		}
-		wcli.CheckboxToggle.superclass.setValue.call(this, value);
+		wcli.field.CheckboxToggle.superclass.setValue.call(this, value);
 	},
 	
 	getValue: function() {
-		var value = wcli.CheckboxToggle.superclass.getValue.call(this);
+		var value = wcli.field.CheckboxToggle.superclass.getValue.call(this);
 		return this.getString(value);
 	}
 });
-
-Ext.reg('togglefield', wcli.CheckboxToggle);
