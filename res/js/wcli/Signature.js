007128384fbcd4499a308dad18d3f9e8be487855
cm1eg <eric.giron@cmfirstgroup.com>
2024-09-26 11:09:39 -0600

    Update Signature.js
    Fix Canvas and Container size for Signature - Ticket 5712

--- res/js/wcli/Signature.js
+++ res/js/wcli/Signature.js
@@ -11,27 +11,14 @@ Ext.define('wcli.Signature',{
 		this.callParent(arguments);
 		this.on("painted", this.onPainted, this);
 		this.isField = true;
-		var me = this;
-		var checkIfPainted = window.setInterval(function(){
-			var control = document.getElementById(me.id)
-			if(control){
-				for (var i = 1; i <= control.children.length - 1; i++){
-    				if (control.children[i].tagName.toLowerCase() === 'canvas'){
-    					clearInterval(checkIfPainted);
-    					return;
-    				}
-    			}
-    			Ext.getCmp(me.id).onPainted();
-    			clearInterval(checkIfPainted);
-			}
-		}, 100);
 	},
 	
 	onPainted: function(){
 		var newCanvas = document.createElement("CANVAS");
-		newCanvas.setAttribute("width", "600px");
-		this.element.dom.style.width = "600px";
-		this.up('container').element.dom.style.width = "600px";
+		var sizewindow = window.innerWidth.toString()+ "px";
+		newCanvas.setAttribute("width", sizewindow);
+		this.element.dom.style.width = sizewindow;
+		this.up('container').element.dom.style.width = sizewindow;
 		this.up('container').element.dom.style.marginLeft = "auto";
 		this.up('container').element.dom.style.marginRight = "auto";
 		this.element.appendChild(newCanvas);
